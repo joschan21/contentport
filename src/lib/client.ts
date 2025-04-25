fb0790e7b4210cc35b0526c7fbe918f78d56c4bb
Josh <josh@upstash.com>
2025-04-25 15:34:44 +0200

    fix: deployment url

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -10,6 +10,7 @@ export const client = createClient<AppRouter>({
 })
 
 function getBaseUrl() {
+  if (typeof window !== "undefined") return window.location.origin
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
   return `http://localhost:3000`
 }
