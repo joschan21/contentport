db0713a9dcf48a0dc9a6842ab5350963a65f79b4
Josh <josh@upstash.com>
2025-04-25 15:46:00 +0200

    fix: middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,9 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
-//   if (process.env.NODE_ENV === "development") {
-//     return NextResponse.next()
-//   }
+  if (process.env.NODE_ENV === "development") {
+    return NextResponse.next()
+  }
 
   const { pathname } = request.nextUrl
 
@@ -21,13 +21,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public folder
-     */
-    "/((?!_next/static|_next/image|favicon.ico|public).*)",
+    "/((?!_next/static|_next/image|public/|favicon.ico|robots.txt|sitemap.xml|manifest.json).*)",
   ],
 }
