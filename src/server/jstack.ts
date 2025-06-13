7d1bc7ee664ece90f6dca4d32eea77cbae0ac6a2
Josh <josh@upstash.com>
2025-06-13 17:20:21 +0200

    chore: add to allowlist

--- src/server/jstack.ts
+++ src/server/jstack.ts
@@ -1,6 +1,6 @@
-import { auth } from "@/lib/auth"
-import { HTTPException } from "hono/http-exception"
-import { jstack } from "jstack"
+import { auth } from '@/lib/auth'
+import { HTTPException } from 'hono/http-exception'
+import { jstack } from 'jstack'
 
 interface Env {
   Bindings: {}
@@ -9,67 +9,69 @@ interface Env {
 export const j = jstack.init<Env>()
 
 const allowlist = [
-  "akashp1712@gmail.com",
-  "akshathg7@gmail.com",
-  "alvarofragosoc@gmail.com",
-  "dtlvan@gmail.com",
-  "harsh@formbricks.com",
-  "hey@omidshabab.com",
-  "jarrerh@gmail.com",
-  "khoaizahmmed@gmail.com",
-  "khvala@macpaw.com",
-  "lindorf85@gmail.com",
-  "liweizhismd@gmail.com",
-  "luca@studiopiccinotti.it",
-  "michel.binkhorst@xs4all.nl",
-  "neske.joscha@gmail.com",
-  "oluwaseunmauwedo@gmail.com",
-  "p.homoky@gmail.com",
-  "priart@gmail.com",
-  "rohitmeshram000@gmail.com",
-  "tommy.roman.hater@gmail.com",
-  "vurukondasaiteja13@gmail.com",
-  "joscha7676@gmail.com",
-  "jcodog@cleoai.cloud",
-  "hola@tomasholtz.com",
-  "jorge@heyjorge.dev",
-  "hello@joshtriedcoding.com",
-  "8020ui@gmail.com",
-  "danielcspaiva@gmail.com",
-  "getoaarm1@gmail.com",
-  "johnyeocx@gmail.com",
-  "joscha7676@gmail.com",
-  "me@nevillebrem.com",
-  "myhappyagency@gmail.com",
-  "ourrahmaan@gmail.com",
-  "ratan.maurya@gmail.com",
-  "stefanbinoj.007@gmail.com",
-  "taikimingqu@gmail.com",
-  "nizabizaher@gmail.com",
-  "jokirillmeerkatz@outlook.de",
-  "2607jojo@gmail.com",
-  "pietro.dev.07@gmail.com",
-  "lucapiccinotti.lp@gmail.com",
-  "rathoursourabh5@gmail.com",
-  "jonathan@fabworks.com",
-  "justin.s.ragland@gmail.com",
-  "shahram.ask.546@gmail.com",
-  "hello@developeratul.com",
-  "thepiyushchandwani@gmail.com",
-  "jdaly2991@gmail.com",
-  "dev.algomax@gmail.com",
-  "vladanilic017@gmail.com"
+  'akashp1712@gmail.com',
+  'akshathg7@gmail.com',
+  'alvarofragosoc@gmail.com',
+  'dtlvan@gmail.com',
+  'harsh@formbricks.com',
+  'hey@omidshabab.com',
+  'jarrerh@gmail.com',
+  'khoaizahmmed@gmail.com',
+  'khvala@macpaw.com',
+  'lindorf85@gmail.com',
+  'liweizhismd@gmail.com',
+  'luca@studiopiccinotti.it',
+  'michel.binkhorst@xs4all.nl',
+  'neske.joscha@gmail.com',
+  'oluwaseunmauwedo@gmail.com',
+  'p.homoky@gmail.com',
+  'priart@gmail.com',
+  'rohitmeshram000@gmail.com',
+  'tommy.roman.hater@gmail.com',
+  'vurukondasaiteja13@gmail.com',
+  'joscha7676@gmail.com',
+  'jcodog@cleoai.cloud',
+  'hola@tomasholtz.com',
+  'jorge@heyjorge.dev',
+  'hello@joshtriedcoding.com',
+  '8020ui@gmail.com',
+  'danielcspaiva@gmail.com',
+  'getoaarm1@gmail.com',
+  'johnyeocx@gmail.com',
+  'joscha7676@gmail.com',
+  'me@nevillebrem.com',
+  'myhappyagency@gmail.com',
+  'ourrahmaan@gmail.com',
+  'ratan.maurya@gmail.com',
+  'stefanbinoj.007@gmail.com',
+  'taikimingqu@gmail.com',
+  'nizabizaher@gmail.com',
+  'jokirillmeerkatz@outlook.de',
+  '2607jojo@gmail.com',
+  'pietro.dev.07@gmail.com',
+  'lucapiccinotti.lp@gmail.com',
+  'rathoursourabh5@gmail.com',
+  'jonathan@fabworks.com',
+  'justin.s.ragland@gmail.com',
+  'shahram.ask.546@gmail.com',
+  'hello@developeratul.com',
+  'thepiyushchandwani@gmail.com',
+  'jdaly2991@gmail.com',
+  'dev.algomax@gmail.com',
+  'vladanilic017@gmail.com',
+  'heywinit@gmail.com',
+  'chriszeuch.cz@gmail.com',
 ]
 
 const authMiddleware = j.middleware(async ({ c, next }) => {
   const session = await auth.api.getSession({ headers: c.req.raw.headers })
 
   if (!session) {
-    throw new HTTPException(401, { message: "Unauthorized" })
+    throw new HTTPException(401, { message: 'Unauthorized' })
   }
 
   if (!allowlist.includes(session.user.email)) {
-    throw new HTTPException(401, { message: "Unauthorized" })
+    throw new HTTPException(401, { message: 'Unauthorized' })
   }
 
   return await next({ user: session.user })
