9ade23a4aa781a38ac0948ca56fb5a81ad0e1aeb
Jason Connolly <jcodog@jconet.co.uk>
2025-06-20 19:47:06 +0100

    feat: Implement stripe api endpoint and checkout creation procedure

---
+++ src/server/routers/stripe-router.ts
@@ -0,0 +1,33 @@
+import { STRIPE_SUBSCRIPTION_DATA } from '@/constants/stripe-subscription'
+import { stripe } from '@/lib/stripe/client'
+import { j, privateProcedure } from '@/server/jstack'
+import type Stripe from 'stripe'
+
+const stripeRouter = j.router({
+  createCheckout: privateProcedure.query(async ({ c, ctx: { user } }) => {
+    try {
+      const url = process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000'
+      const customerSearch = await stripe.customers.search({
+        query: `email:"${user.email}"`,
+      })
+      let customer = customerSearch.data[0] as Stripe.Customer | undefined
+      if (!customer) {
+        customer = await stripe.customers.create({ name: user.name, email: user.email })
+      }
+      const checkout = await stripe.checkout.sessions.create({
+        mode: 'subscription',
+        billing_address_collection: 'auto',
+        line_items: [{ price: STRIPE_SUBSCRIPTION_DATA.priceId!, quantity: 1 }],
+        customer: customer.id,
+        success_url: `${url}/success`,
+        cancel_url: `${url}/cancel`,
+        payment_method_types: ['card', 'link', 'paypal'],
+      })
+      return c.json({ url: checkout.url ?? null })
+    } catch (error: unknown) {
+      console.error('Error creating checkout session:', error)
+      const message = error instanceof Error ? error.message : 'Unknown error occurred'
+      return c.json({ error: message })
+    }
+  }),
+})
