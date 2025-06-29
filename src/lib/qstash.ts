6d81ba58f352f7b5b964f129bf84281da18da012
Josh <josh@upstash.com>
2025-06-29 21:41:59 +0200

    fix

--- src/lib/qstash.ts
+++ src/lib/qstash.ts
@@ -1,7 +1,5 @@
-import { getBaseUrl } from '@/constants/base-url'
 import { Client } from '@upstash/qstash'
 
 export const qstash = new Client({
   token: process.env.QSTASH_TOKEN,
-  baseUrl: getBaseUrl(),
 })
