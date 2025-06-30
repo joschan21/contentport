aa3d79c285773f1846056d3ccd95afb544ab8b93
Josh <josh@upstash.com>
2025-06-30 17:17:22 +0200

    i hate vercel urls

--- src/constants/base-url.ts
+++ src/constants/base-url.ts
@@ -1,5 +1,5 @@
 export const getBaseUrl = () => {
   if (typeof window !== 'undefined') return window.location.origin
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
+  if (process.env.NODE_ENV === 'production') return `https://contentport.io`
   return `http://localhost:3000`
 }
