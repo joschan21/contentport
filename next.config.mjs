223d2fec35ff666cabdba64be51d3d6dc8bb4138
Josh <josh@upstash.com>
2025-07-08 11:09:30 +0200

    enable posthog sourcemaps

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withPostHogConfig } from '@posthog/nextjs-config'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['pdf-parse'],
@@ -17,11 +19,22 @@ const nextConfig = {
         source: '/ingest/decide',
         destination: 'https://eu.i.posthog.com/decide',
       },
-    ];
+    ]
   },
 
   // This is required to support PostHog trailing slash API requests
   skipTrailingSlashRedirect: true,
-};
+}
 
-export default nextConfig;
+export default withPostHogConfig(nextConfig, {
+  personalApiKey: process.env.POSTHOG_API_KEY, // Personal API Key
+  envId: process.env.POSTHOG_ENV_ID, // Environment ID
+  host: process.env.NEXT_PUBLIC_POSTHOG_HOST, // (optional), defaults to https://us.posthog.com
+  sourcemaps: {
+    // (optional)
+    enabled: true, // (optional) Enable sourcemaps generation and upload, default to true on production builds
+    project: 'my-application', // (optional) Project name, defaults to repository name
+    version: '1.0.0', // (optional) Release version, defaults to current git commit
+    deleteAfterUpload: true, // (optional) Delete sourcemaps after upload, defaults to true
+  },
+})
