c216f41e71294f4d08b9329c38ef240aa89d8ff4
Josh <josh@upstash.com>
2025-07-10 14:44:56 +0200

    fix: order

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -13,7 +13,7 @@ export const authClient = createAuthClient({
 })
 
 function getBaseUrl() {
+  if (process.env.NODE_ENV === 'production') return `https://contentport.io`
   if (typeof window !== 'undefined') return window.location.origin
-  if (process.env.NODE_ENV === "production") return `https://contentport.io`
   return `http://localhost:3000`
 }
