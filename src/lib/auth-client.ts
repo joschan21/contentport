4e6a59f7f7805b5368c0d06ec1649bb3d2daee38
Josh <josh@upstash.com>
2025-08-12 16:07:34 +0200

    feat: dynamic base url config

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,6 +2,12 @@ import { createAuthClient } from 'better-auth/react'
 import { inferAdditionalFields } from 'better-auth/client/plugins'
 
 export const authClient = createAuthClient({
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? process.env.BETTER_AUTH_URL
+        ? process.env.BETTER_AUTH_URL
+        : `https://${process.env.VERCEL_BRANCH_URL}`
+      : 'http://localhost:3000',
   plugins: [
     inferAdditionalFields({
       user: {
