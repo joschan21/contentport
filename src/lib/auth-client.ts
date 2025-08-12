845bd1a4ef29dc9b984d8a029730e64735730806
Josh <josh@upstash.com>
2025-08-12 16:41:23 +0200

    refactor: set feature branch base url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,6 +2,7 @@ import { createAuthClient } from 'better-auth/react'
 import { inferAdditionalFields } from 'better-auth/client/plugins'
 
 export const authClient = createAuthClient({
+  baseURL: 'https://contentport-git-feat-thread-support-joschan21s-projects.vercel.app',
   plugins: [
     inferAdditionalFields({
       user: {
