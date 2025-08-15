e75e9b74feb25dd8a60aa68ea4bed38c7755e5c0
Josh <josh@upstash.com>
2025-08-15 11:14:40 +0200

    fix: support preview environment urls

--- src/constants/base-url.ts
+++ src/constants/base-url.ts
@@ -1,5 +1,10 @@
 export const getBaseUrl = () => {
   if (typeof window !== 'undefined') return window.location.origin
+
+  if (process.env.VERCEL_ENV === 'preview' && process.env.VERCEL_BRANCH_URL) {
+    return `https://${process.env.VERCEL_BRANCH_URL}`
+  }
+
   if (process.env.NODE_ENV === 'production') return `https://contentport.io`
   return `http://localhost:3000`
 }
