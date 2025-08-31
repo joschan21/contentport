228858f067df0e38289a8f6a4aefcc3020f449bc
Josh <josh@upstash.com>
2025-08-31 09:19:54 +0200

    refactor: remove redirect on previews

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -104,15 +104,15 @@ export const auth = betterAuth({
       secure: true,
     },
   },
-  // hooks: {
-  //   after: createAuthMiddleware(async (ctx) => {
-  //     const session = ctx.context.newSession
+  hooks: {
+    after: createAuthMiddleware(async (ctx) => {
+      const session = ctx.context.newSession
 
-  //     if (session) {
-  //       ctx.redirect('/studio')
-  //     } else {
-  //       ctx.redirect('/')
-  //     }
-  //   }),
-  // },
+      if (session) {
+        ctx.redirect('/studio')
+      } else {
+        ctx.redirect('/')
+      }
+    }),
+  },
 })
