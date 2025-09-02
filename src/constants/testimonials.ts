983b120333c0b1752527d94247a55686e9474262
jomeerkatz <jokirillmeerkatz@outlook.de>
2025-09-02 14:19:19 +0200

    style: testimonial section adjustments

--- src/constants/testimonials.ts
+++ src/constants/testimonials.ts
@@ -1,147 +1,139 @@
 export interface Testimonial {
-    name: string;
-    username: string;
-    body: string;
-    img: string;
-    href: string;
-    title?: string;
+  name: string;
+  username: string;
+  body: string;
+  img: string;
+  href: string;
+  title?: string;
 }
 
 export const testimonials: Testimonial[] = [
-    {
-        name: "abdush",
-        username: "@abdushbag",
-        body: "really impressed by @contentport. amazing work @joshtriedcoding and @jomeerkatz you guys crushed it!",
-        img: "/images/user/abdush_128.jpg",
-        href: "https://x.com/abdushbag/status/1945037240704413811",
-        title: "SWE @uptstash"
-      },
-      {
-        name: "Iza",
-        username: "@izadoesdev",
-        body: "contentport actually makes writing tweets fun \n like i can just focus on the ideas instead of fighting the interface \nfeels like it was built by someone who actually uses twitter",
-        img: "/images/user/iza_128.jpg",
-        href: "https://x.com/izadoesdev/status/1947722887109996636",
-        title: "Building @trydatabuddy"
-      },
-      {
-        name: "aasim - oss/acc",
-        username: "@BhatAasim9",
-        body: "just got early access to @contentport and holy shit this is good.",
-        img: "/images/user/aasim_128.jpg",
-        href: "https://x.com/BhatAasim9/status/1935367855999951200",
-        title: "Engineer"
-      },
-      {
-        name: "Daniel Zaremba",
-        username: "@dzrmb",
-        body: `First post made with @contentport. \n Feels clean, fast af, and simple to write in. \n I have no excuses to not grow my X account anymore! 🙈`,
-        img: "/images/user/daniel_128.jpg",
-        href: "https://x.com/dzrmb/status/1952005140652712330",
-        title: "Consultant"
-      },
-      {
-        name: "ahmet",
-        username: "@bruvimtired",
-        body: "just tried @contentport for the first time and wow this is clean 👀",
-        img: "/images/user/ahmet_128.jpg",
-        href: "https://x.com/bruvimtired/status/1934572161349325270",
-        title: "Founding Engineer @mail0dotcom"
-      },
-      {
-        name: "Ray Fernando",
-        username: "@RayFernando1337",
-        body: "This is a a baaaaanger!!",
-        img: "/images/user/ray_128.jpg",
-        href: "https://x.com/RayFernando1337/status/1947477872018620548",
-        title: "Engineer"
-      },
-      {
-        name: "Dominik Koch",
-        username: "@DominikDoesDev",
-        body: "hey @joshtriedcoding I love @contentport",
-        img: "/images/user/dominik_128.jpg",
-        href: "https://x.com/DominikDoesDev/status/1949870189832523828",
-        title: "Building @usemarblecms"
-      },
-      {
-        name: "Vikas Wakde",
-        username: "@VikasWakde42",
-        body: "Loving ContentPort by @joshtriedcoding \n\nThe UI/UX is amazing and the features are so clean\n\nThank you for building this open source! \n\nPS: this tweet is drafted and posted by @contentport",
-        img: "/images/user/vikas_128.jpg",
-        href: "https://x.com/VikasWakde42/status/1951668308995911725",
-        title: "SWE"
-      },
-      {
-        name: "Vlad",
-        username: "@deifosv",
-        body: "This looks nice, I like that is not about writing the posts for me but helping me shape what I want to say. Looking forward to this.",
-        img: "/images/user/vlad_128.jpg",
-        href: "https://x.com/deifosv/status/1947380624538620055",
-        title: "SWE"
-      },
-      {
-        name: "adam ghaida",
-        username: "@adamghaida",
-        body: "love this! always crushing it",
-        img: "/images/user/adam_128.jpg",
-        href: "https://x.com/adamghaida/status/1915444024040685930",
-        title: "SWE @mail0dotcom"
-      },
-      {
-        name: "nizzy",
-        username: "@nizzyabi",
-        body: "LETS GO",
-        img: "/images/user/nizzy_128.jpg",
-        href: "https://x.com/nizzyabi/status/1916176163690742068",
-        title: "Co-founder of @mail0dotcom"
-      },
-      {
-        name: "Michael",
-        username: "@michael_chomsky",
-        body: "@contentport is so fire it's gonna reach 1m ARR < 1 year",
-        img: "/images/user/michael_128.jpg",
-        href: "https://x.com/michael_chomsky/status/1936639940735225873",
-        title: "Devrel @inference.net"
-      },
-      {
-        name: "Nikita",
-        username: "@nikitadrokin",
-        body: ".@contentport has the cleanest buttons I've seen in any saas",
-        img: "/images/user/nikita_128.jpg",
-        href: "https://x.com/nikitadrokin/status/1953237984201064683",
-        title: "Web Dev"
-      },
-      {
-        name: "Muhammad Zafar",
-        username: "@zafarbuildzz",
-        body: "Trying out @contentport made by @joshtriedcoding. It's really good and OPEN SOURCE 🤤",
-        img: "/images/user/muhammad_128.jpg",
-        href: "https://x.com/zafarbuildzz/status/1950768228919124025",
-        title: "SWE"
-      },
-      {
-        name: "miaugladiator11",
-        username: "@miaugladiator1",
-        body: "the ui for contentport is just too good. \n simplicity > complexity. Clean design, intuitive flow, and zero clutter",
-        img: "/images/user/miaugladiator11_128.jpg",
-        href: "https://x.com/miaugladiator1/status/1950134204118806681",
-        title: "SWE"
-      },
-      {
-        name: "Bilal Bakr",
-        username: "@bil0090",
-        body: "Man, that's incredible, can't wait to try it! When is it going to be available?",
-        img: "/images/user/bilal_128.jpg",
-        href: "https://x.com/bil0090/status/1920143901819572402",
-        title: "SWE & Co-founder @Calendaty"
-      },
-      {
-        name: "G",
-        username: "@blackevilgoblin",
-        body: "ContentPort is where I write my best tweets. Clean interface, zero distractions, just me and my ideas. Writing feels good again.",
-        img: "/images/user/g_128.jpg",
-        href: "https://x.com/blackevilgoblin/status/1927087174681329698",
-        title: "Agency owner & YouTuber"
-      }
+  {
+    name: "abdush",
+    username: "@abdushbag",
+    body: "really impressed by @contentport. amazing work @joshtriedcoding and @jomeerkatz you guys crushed it!",
+    img: "/images/user/abdush_128.jpg",
+    href: "https://x.com/abdushbag/status/1945037240704413811",
+    title: "SWE @uptstash"
+  },
+  {
+    name: "Iza",
+    username: "@izadoesdev",
+    body: "contentport actually makes writing tweets fun \n like i can just focus on the ideas instead of fighting the interface \nfeels like it was built by someone who actually uses twitter",
+    img: "/images/user/iza_128.jpg",
+    href: "https://x.com/izadoesdev/status/1947722887109996636",
+    title: "Building @trydatabuddy"
+  },
+  {
+    name: "aasim - oss/acc",
+    username: "@BhatAasim9",
+    body: "just got early access to @contentport and holy shit this is good.",
+    img: "/images/user/aasim_128.jpg",
+    href: "https://x.com/BhatAasim9/status/1935367855999951200",
+    title: "Engineer"
+  },
+  {
+    name: "Daniel Zaremba",
+    username: "@dzrmb",
+    body: `First post made with @contentport. \n Feels clean, fast af, and simple to write in. \n I have no excuses to not grow my X account anymore! 🙈`,
+    img: "/images/user/daniel_128.jpg",
+    href: "https://x.com/dzrmb/status/1952005140652712330",
+    title: "Consultant"
+  },
+  {
+    name: "ahmet",
+    username: "@bruvimtired",
+    body: "just tried @contentport for the first time and wow this is clean 👀",
+    img: "/images/user/ahmet_128.jpg",
+    href: "https://x.com/bruvimtired/status/1934572161349325270",
+    title: "Founding Engineer @mail0dotcom"
+  },
+  {
+    name: "Bilal Bakr",
+    username: "@bil0090",
+    body: "Man, that's incredible, can't wait to try it! When is it going to be available?",
+    img: "/images/user/bilal_128.jpg",
+    href: "https://x.com/bil0090/status/1920143901819572402",
+    title: "SWE & Co-founder @Calendaty"
+  },
+  {
+    name: "Ray Fernando",
+    username: "@RayFernando1337",
+    body: "This is a a baaaaanger!!",
+    img: "/images/user/ray_128.jpg",
+    href: "https://x.com/RayFernando1337/status/1947477872018620548",
+    title: "Engineer"
+  },
+  {
+    name: "Dominik Koch",
+    username: "@DominikDoesDev",
+    body: "hey @joshtriedcoding I love @contentport",
+    img: "/images/user/dominik_128.jpg",
+    href: "https://x.com/DominikDoesDev/status/1949870189832523828",
+    title: "Building @usemarblecms"
+  },
+  {
+    name: "Vlad",
+    username: "@deifosv",
+    body: "This looks nice, I like that is not about writing the posts for me but helping me shape what I want to say. Looking forward to this.",
+    img: "/images/user/vlad_128.jpg",
+    href: "https://x.com/deifosv/status/1947380624538620055",
+    title: "SWE"
+  },
+  {
+    name: "adam ghaida",
+    username: "@adamghaida",
+    body: "love this! always crushing it",
+    img: "/images/user/adam_128.jpg",
+    href: "https://x.com/adamghaida/status/1915444024040685930",
+    title: "SWE @mail0dotcom"
+  },
+  {
+    name: "nizzy",
+    username: "@nizzyabi",
+    body: "LETS GO",
+    img: "/images/user/nizzy_128.jpg",
+    href: "https://x.com/nizzyabi/status/1916176163690742068",
+    title: "Co-founder of @mail0dotcom"
+  },
+  {
+    name: "Michael",
+    username: "@michael_chomsky",
+    body: "@contentport is so fire it's gonna reach 1m ARR < 1 year",
+    img: "/images/user/michael_128.jpg",
+    href: "https://x.com/michael_chomsky/status/1936639940735225873",
+    title: "Devrel @inference.net"
+  },
+  {
+    name: "Muhammad Zafar",
+    username: "@zafarbuildzz",
+    body: "Trying out @contentport made by @joshtriedcoding. It's really good and OPEN SOURCE 🤤",
+    img: "/images/user/muhammad_128.jpg",
+    href: "https://x.com/zafarbuildzz/status/1950768228919124025",
+    title: "SWE"
+  },
+  {
+    name: "miaugladiator11",
+    username: "@miaugladiator1",
+    body: "the ui for contentport is just too good. \n simplicity > complexity. Clean design, intuitive flow, and zero clutter",
+    img: "/images/user/miaugladiator11_128.jpg",
+    href: "https://x.com/miaugladiator1/status/1950134204118806681",
+    title: "SWE"
+  },
+  {
+    name: "Vikas Wakde",
+    username: "@VikasWakde42",
+    body: "Loving ContentPort by @joshtriedcoding \n\nThe UI/UX is amazing and the features are so clean\n\nThank you for building this open source! \n\nPS: this tweet is drafted and posted by @contentport",
+    img: "/images/user/vikas_128.jpg",
+    href: "https://x.com/VikasWakde42/status/1951668308995911725",
+    title: "SWE"
+  },
+  {
+    name: "G",
+    username: "@blackevilgoblin",
+    body: "ContentPort is where I write my best tweets. Clean interface, zero distractions, just me and my ideas. Writing feels good again.",
+    img: "/images/user/g_128.jpg",
+    href: "https://x.com/blackevilgoblin/status/1927087174681329698",
+    title: "Agency owner & YouTuber"
+  }
 ]
